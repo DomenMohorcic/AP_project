04c52ded4921d48a2bfc396a28447d146700cdd8
LarsenCC <47952756+LarsenCC@users.noreply.github.com>
2021-03-22 13:10:15 +0100

    nav bar + first fragment

--- StonkTracker/app/src/main/java/com/project/stonktracker/MainActivity.kt
+++ StonkTracker/app/src/main/java/com/project/stonktracker/MainActivity.kt
@@ -2,10 +2,34 @@ package com.project.stonktracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.project.stonktracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var nav: BottomNavigationView
+    private lateinit var selectedFragment: Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        nav = binding.bottomNavigationView
+        nav.setOnNavigationItemReselectedListener {
+            if (it.itemId == R.id.nav_home) {
+                selectedFragment = MainFragment()
+            }
+
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragment, selectedFragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
     }
+
 }
---
+++ StonkTracker/app/src/main/java/com/project/stonktracker/MainFragment.kt
@@ -0,0 +1,45 @@
+package com.project.stonktracker
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.project.stonktracker.databinding.MainFragmentBinding
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    private var _binding: MainFragmentBinding? = null
+    // This property is only valid between onCreateView and onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
