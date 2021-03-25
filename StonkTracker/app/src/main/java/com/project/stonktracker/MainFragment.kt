3e6a3ee530ef2777e32329c94c78e4694318c84c
LarsenCC <47952756+LarsenCC@users.noreply.github.com>
2021-03-25 08:55:23 +0100

    malenkosti popravljene

--- StonkTracker/app/src/main/java/com/project/stonktracker/MainFragment.kt
+++ StonkTracker/app/src/main/java/com/project/stonktracker/MainFragment.kt
@@ -34,16 +34,16 @@ class MainFragment : Fragment() {
 
         val view: View = inflater.inflate(R.layout.main_fragment, container, false)
 
-        recyclerView = view.findViewById(R.id.recyclerViewMain)
+        recyclerView = binding.recyclerViewMain
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = GridLayoutManager(view.context, 1)
         recyclerView.adapter = MainFragmentAdapter(arrayListOf(
             StockInfo("T", "AT&T", "att.com", 20),
-            StockInfo("APPL", "Apple Inc.", "apple.com", 50),
+            StockInfo("AAPL", "Apple Inc.", "apple.com", 50),
             StockInfo("STB", "Starbucks", "starbucks.com", 45)
         ))
 
-        return view //binding.root
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -59,36 +59,3 @@ class MainFragment : Fragment() {
     }
 }
 
-class MainFragmentAdapter(private val stockData: ArrayList<StockInfo>) : RecyclerView.Adapter<MainFragmentAdapter.ViewHolder>() {
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val logoImage: ImageView = view.findViewById(R.id.imageViewLogo)
-        val nameText: TextView = view.findViewById(R.id.nameText)
-        val sharesText: TextView = view.findViewById(R.id.totalSharesCurrentPrice)
-        // val priceText: TextView = view.findViewById(R.id.totalSharesCurrentPrice)
-        val valueText: TextView = view.findViewById(R.id.totalValue)
-        val changeText: TextView = view.findViewById(R.id.sharesPL)
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.portfolio_view, parent, false)
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val si: StockInfo = stockData[position]
-        val price = 450 //API klic
-        Glide.with(holder.logoImage.context as Activity)
-             .load("https://logo.clearbit.com/${si.webURL}")
-             .into(holder.logoImage)
-        holder.nameText.text = si.full_name
-        holder.sharesText.text = si.shares.toString()
-        // holder.priceText.text = price.toString()
-        holder.valueText.text = (price * si.shares).toString()
-        holder.changeText.text = (price * si.shares - si.shares).toString() // * price bought
-    }
-
-    override fun getItemCount(): Int {
-        return stockData.size
-    }
-}
---
+++ StonkTracker/app/src/main/java/com/project/stonktracker/MainFragmentAdapter.kt
@@ -0,0 +1,46 @@
+package com.project.stonktracker
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class MainFragmentAdapter(private val stockData: ArrayList<StockInfo>) : RecyclerView.Adapter<MainFragmentAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val logoImage: ImageView = view.findViewById(R.id.imageViewLogo)
+        val nameText: TextView = view.findViewById(R.id.nameText)
+        val sharesText: TextView = view.findViewById(R.id.totalSharesCurrentPrice)
+        // val priceText: TextView = view.findViewById(R.id.totalSharesCurrentPrice)
+        val valueText: TextView = view.findViewById(R.id.totalValue)
+        val changeText: TextView = view.findViewById(R.id.sharesPL)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.portfolio_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val si: StockInfo = stockData[position]
+
+        val price = 450 // TODO API call
+
+        Glide.with(holder.logoImage.context as Activity)
+            .load("https://logo.clearbit.com/${si.webURL}")
+            .into(holder.logoImage)
+        holder.nameText.text = si.full_name
+        holder.sharesText.text = si.shares.toString()
+        // holder.priceText.text = price.toString()
+        holder.valueText.text = (price * si.shares).toString()
+        holder.changeText.text = (price * si.shares - si.shares).toString() // * price bought
+    }
+
+    override fun getItemCount(): Int {
+        return stockData.size
+    }
+}
