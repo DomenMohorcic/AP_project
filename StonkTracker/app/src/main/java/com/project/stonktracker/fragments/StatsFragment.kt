6a3fad62e0090c5371ebc0f933312bdcd76377c8
Domen Mohorcic <d.mohorcic@gmail.com>
2021-04-03 13:42:12 +0200

    PieChart working

--- StonkTracker/app/src/main/java/com/project/stonktracker/fragments/StatsFragment.kt
+++ StonkTracker/app/src/main/java/com/project/stonktracker/fragments/StatsFragment.kt
@@ -1,19 +1,27 @@
 package com.project.stonktracker.fragments
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.project.stonktracker.R
 import com.project.stonktracker.TransactionFragment
-import com.project.stonktracker.databinding.FragmentStatsBinding
-import com.project.stonktracker.databinding.TransactionFragmentBinding
+import com.project.stonktracker.databinding.StatsFragmentBinding
 import com.project.stonktracker.viewmodels.Company
 import com.project.stonktracker.viewmodels.FragmentVM
 import com.project.stonktracker.viewmodels.StocksVM
+import java.text.DecimalFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
@@ -30,7 +38,7 @@ class StatsFragment : Fragment() {
     private lateinit var valuePieChart: PieChart
 
     // binding
-    private var _binding: FragmentStatsBinding? = null
+    private var _binding: StatsFragmentBinding? = null
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
@@ -40,15 +48,53 @@ class StatsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         container?.removeAllViews() // else previous fragment is visible in background
-        _binding = FragmentStatsBinding.inflate(inflater, container, false)
+        _binding = StatsFragmentBinding.inflate(inflater, container, false)
 
         valuePieChart = binding.valuePieChart
+        valuePieChart.description.text = ""
+        //valuePieChart.holeRadius = 50f
+        //valuePieChart.transparentCircleRadius = 55f
+        valuePieChart.legend.isEnabled = false
+
+        stocksVM.getStocks().observe(viewLifecycleOwner, { stocks ->
+            val entries = ArrayList<PieEntry>()
+            var value = 0.0
+            for(stock in stocks) {
+                entries.add(PieEntry((stock.shares*stock.last_close).toFloat(), stock.ticker))
+                value += stock.shares*stock.last_close
+            }
+            Log.i("pie", value.toString())
+            val pieDataSet = PieDataSet(entries, "")
+            pieDataSet.colors = getRainbowColors(stocks.size) //ColorTemplate.COLORFUL_COLORS.toCollection(ArrayList())
+            pieDataSet.valueFormatter = MyValueFormatter()
+            val pieData = PieData(pieDataSet)
+            valuePieChart.data = pieData
+            valuePieChart.centerText = "$"+value.toString() // TODO doesn't work
+            valuePieChart.invalidate()
+        })
 
         return binding.root
+    }
+
+    // TODO make it to create rainbow
+    private fun getRainbowColors(number: Int): ArrayList<Int> {
+        var c = ArrayList<Int>()
+        for(i in 0 until number) {
+            c.add(Color.rgb((0..255).random(), (0..255).random(), (0..255).random()))
+        }
+        return c
     }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+}
+
+class MyValueFormatter : ValueFormatter() {
+    private val format = DecimalFormat("###,##0.0")
+
+    override fun getPieLabel(value: Float, pieEntry: PieEntry?): String {
+        return "$${format.format(value)}"
     }
 }
